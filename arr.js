82f57a78bab0ae973bc76c692b9f40ecfaf7383f
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-11-09 13:16:32 -0500

    s

--- arr.js
+++
@@ -1,9 +0,0 @@
-const item [
-{
-name: Raspberry Eclair
-}
-
-
-
-
-];
---
+++ vite-project/arr.js
@@ -0,0 +1,73 @@
+const item = [
+    {
+        name: "Raspberry Eclair",
+        img: "https://static01.nyt.com/images/2019/01/30/dining/ss-raspberry-and-cream-eclairs/merlin_149765565_a2e74bc4-f85d-409b-ab6a-8517b2",
+        price: 4.50,
+        mostpop: true,
+        seasonal: false,
+        bev: false,
+    },
+
+    {
+        name: "Pain au Chocolat",
+        img: "https://www.thespruceeats.com/thmb/D9po8nbJ09H8YjW_cFcpAQH7Q5I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-easy-pain-au-chocolat-recipe-1374869-hero-C-e1d7f1e947094ab39c3ab063039202e0.jpg",
+        price: 4,
+        mostpop: true,
+        seasonal: false,
+        bev: false,
+    },
+
+    {
+        name: "Tiramisu Cake Slice",
+        img: "https://thebigmansworld.com/wp-content/uploads/2022/10/tiramisu-cake-recipe.jpg",
+        price: 5,
+        mostpop: true,
+        seasonal: false,
+        bev: false,
+    },
+    {
+        name: "Pumpkin Spice Cheesecake Slice",
+        img: "https://www.sugarhero.com/wp-content/uploads/2021/11/pumpkin-spice-cheesecake-3.jpg",
+        price: 5.50,
+        rating: 6.50,
+        mostpop: false,
+        seasonal: true,
+        bev: false,
+    },
+    {
+        name: "White Chocolate Peppermint Cookies",
+        img: "https://celebratingsweets.com/wp-content/uploads/2021/12/Peppermint-Cookies-6.jpg",
+        price: 4.50,
+        rating: 5,
+        mostpop: false,
+        seasonal: true,
+        bev: false,
+    },
+    {
+        name: "Peppermint Mocha Latte",
+        img: "https://www.thereciperebel.com/wp-content/uploads/2018/11/peppermint-mocha-www.thereciperebel.com-1200-8-of-12.jpg",
+        price: 3.5,
+        rating: 4,
+        mostpop: true,
+        seasonal: true,
+        bev: true,
+    },
+    {
+        name: "Red Velvet Battenberg Cake Slice",
+        img: "https://bakefromscratch.com/wp-content/uploads/2015/11/Bobs-Battenberg947JBweb-696x557.jpg",
+        price: 6,
+        rating: 4,
+        mostpop: true,
+        seasonal: true,
+    },
+    {
+        name: "Red Velvet Battenberg Cake Slice",
+        img: "https://bakefromscratch.com/wp-content/uploads/2015/11/Bobs-Battenberg947JBweb-696x557.jpg",
+        price: 6,
+        rating: 4,
+        mostpop: false,
+        seasonal: false,
+    },
+
+
+];
