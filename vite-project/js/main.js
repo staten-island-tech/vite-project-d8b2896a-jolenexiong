5b696d0b3dddb7aca636a13a94648b2d8964ca01
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-11-13 13:08:34 -0500

    e

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,24 @@
 import "../css/style.css";
 import { food } from "./food";
+
+function displayall (){
+
+
+}
+displayall;
+
+
+("#app").innerHTML = `
+<h1>Welcome to Jolene's Bakery Wesbite!"</h1>
+`;
 console.log(food);
 
-const DOMSelectors= {
-    mostpop: document.querySelector("#popbtn"),
-    star: document.querySelector("#ratingbtn"),
-
-
+const DOMSelectors = {
+pop: document.querySelector("#mostpopbtn"),
+star: document.querySelector("#popbtn"),
+bev: document.querySelector("#bevbtn"),
+szn:docuument.querySelector("#sznbtn"),
 };
 
-
+function rating ();
+    document.querySelector(".pop").forEach((btn))
