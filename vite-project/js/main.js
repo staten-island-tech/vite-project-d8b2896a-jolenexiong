0f86c42b26f796e049a94560a3491daf588cea67
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-11-14 13:14:54 -0500

    eeee

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,24 +1,33 @@
 import "../css/style.css";
 import { food } from "./food";
 
-function displayall (){
+function displayall() {
+    food.forEach((T) => app.parentElement.insertAdjacentHtml =
+        "afterbegin",
+        `<div class="card">
+<h2>${T.name}</h2>
+<img src = "${picture.value}">
+<h2>${T.rating}</h2>
+<h2>${T.price}</h2>
+</div>`
 
-
+    );
 }
 displayall;
 
+console.log(food)
 
-("#app").innerHTML = `
-<h1>Welcome to Jolene's Bakery Wesbite!"</h1>
-`;
-console.log(food);
+
+
+
 
 const DOMSelectors = {
-pop: document.querySelector("#mostpopbtn"),
-star: document.querySelector("#popbtn"),
-bev: document.querySelector("#bevbtn"),
-szn:docuument.querySelector("#sznbtn"),
+    pop: document.querySelector("#mostpopbtn"),
+    bev: document.querySelector("#bevbtn"),
+    szn: document.querySelector("#sznbtn"),
 };
 
-function rating ();
+function rating() {
     document.querySelector(".pop").forEach((btn))
+
+}
