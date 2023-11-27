b3683ebf25cdf1fe93594657017527f109f7ed62
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-11-27 13:16:03 -0500

    w

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -21,7 +21,7 @@ console.log(food)
 
 
 const DOMSelectors = {
-    pop: document.querySelector("#mostpopbtn"),
+    pop: document.querySelector("#popbtn"),
     bev: document.querySelector("#bevbtn"),
     szn: document.querySelector("#sznbtn"),
 
@@ -31,6 +31,15 @@ const DOMSelectors = {
 function pop() {
     document.querySelector(".pop").forEach((btn) => btn.addEventListener('click', function (event) {
         food.filter((food) => food.mostpop === true)
+            .forEach((food) => food.insertAdjacentHTML("beforeend",
+                `<div class="card">
+    <h2 class="name">${meal.name}</h2>
+    <img src = "${meal.img}"class="pic">
+    <h2>${meal.price}</h2>
+    </div>`
+
+            ))
     })
     );
 }
+ pop()
