d115fbde024f5f9f2ee58605fa00cd3feb4b3917
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-12-08 13:04:04 -0500

    DONE

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,7 +2,7 @@ import "../css/style.css";
 import { food } from "./food";
 
 function displayall() {
-    let app = document.querySelector("#app")
+    const app = document.querySelector("#app")
     food.forEach((meal) => app.insertAdjacentHTML(
         "beforeend",
         `<div class="card">
@@ -14,14 +14,15 @@ function displayall() {
 }
 
 function displayone(meal) {
+    meal.forEach((x) => {
 DOMSelectors.app.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-<h2 class="name">${meal.name}</h2>
-<img src = "${meal.img}"class="pic">
-<h2>${meal.price}</h2>
-</div>`
-)
+    <h2 class="name">${x.name}</h2>
+    <img src = "${x.img}"class="pic">
+    <h2>${x.price}</h2>
+    </div>`
+)})
 }
 
 
@@ -75,30 +76,41 @@ DOMSelectors.all.addEventListener("click", function () {
 let sznalbutton = [DOMSelectors.Cszn, DOMSelectors.Fszn]
 
 sznalbutton.forEach((btn)=> btn.addEventListener("click", function (){
-    let season = btn.textContent.toLowerCase()
-    let meal = food.filter((whichszn) => whichszn.seasonal === 'Christmas')
+    const season = btn.textContent
+    console.log(season)
+    const meal = food.filter((whichszn) => whichszn.seasonal === season)
     console.log(meal)
     clearFields();
     displayone(meal);
 }))
 
-
-
-
-
-
-DOMSelectors.bev.addEventListener("click", function () {
-    const bevs = food.filter((meal) => meal.bev === true);
+DOMSelectors.bev.addEventListener("click", function (){
+    const meal = food.filter((yes)=>yes.bev===true);
+    console.log(meal)
     clearFields();
-    bevs.forEach((meal) => DOMSelectors.app.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card">
-        <h2 class="name">${meal.name}</h2>
-        <img src = "${meal.img}"class="pic">
-        <h2>${meal.price}</h2>
-        </div>`
-    ))
+    displayone(meal);
+
 })
+
+
+
+
+// DOMSelectors.bev.addEventListener("click", function () {
+//     const bevs = food.filter((meal) => meal.bev === true);
+//     clearFields();
+//     bevs.forEach((meal) => DOMSelectors.app.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="card">
+//         <h2 class="name">${meal.name}</h2>
+//         <img src = "${meal.img}"class="pic">
+//         <h2>${meal.price}</h2>
+//         </div>`
+//     ))
+// })
+
+
+
+
 DOMSelectors.theme.addEventListener("click", function () {
     if (document.body.classList.contains("pink")) {
         document.body.classList.add("blue");
